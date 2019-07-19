2afc820ebf0d050f7bbe78867efe981d53bda331
dmitriiii <dimitriy1988wl@gmail.com>
2019-07-19 10:38:34 +0600

    clear

--- functions.php
+++ functions.php
@@ -136,18 +136,11 @@ if( in_array(get_site_url(), $k8_arrr) ){
 	 * Add REST API support to an already registered post type.
 	 */
 	add_filter( 'register_post_type_args', 'my_post_type_args', 10, 2 );
-
 	function my_post_type_args( $args, $post_type ) {
-
-	    if ( 'affcoups_vendor' === $post_type ) {
-	        $args['show_in_rest'] = true;
-
-	        // Optionally customize the rest_base or rest_controller_class
-	        // $args['rest_base']             = 'books';
-	        // $args['rest_controller_class'] = 'WP_REST_Posts_Controller';
-	    }
-
-	    return $args;
+    if ( 'affcoups_vendor' === $post_type ) {
+      $args['show_in_rest'] = true;
+    }
+    return $args;
 	}
 
 
