69f236ca8ed5d2eb331b253ef7d9cf665c9fb3bc
dmitriiii <dimitriy1988wl@gmail.com>
2019-07-24 10:56:00 +0600

    pravka5

--- functions.php
+++ functions.php
@@ -185,11 +185,7 @@ if( in_array(get_site_url(), $k8_arrr) ){
 		$post_id = $object['id'];
 		$affcoups_vendor_image = get_post_meta( $post_id, 'affcoups_vendor_image', true );
 		$arr = wp_get_attachment_image_src( $affcoups_vendor_image, 'medium_large');
-		$ret = array(
-			'url' => $arr[0],
-			'name' => get_the_title($affcoups_vendor_image)
-		);
-		return $ret;
+		return $arr[0];
 	}
 
 
