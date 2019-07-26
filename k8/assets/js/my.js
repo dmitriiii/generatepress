d2a8f6d261521842f55b145df95fbfcccc4181a3
dmitriiii <dimitriy1988wl@gmail.com>
2019-07-26 19:39:33 +0600

    pravka11

--- k8/assets/js/my.js
+++ k8/assets/js/my.js
@@ -1,16 +1,16 @@
 jQuery(document).ready(function($){
 	// Scroll to block
-	$('body').on('click', '.k8-repl__link', function(e) {
-		e.preventDefault();
-		var blck = $(this).attr('href'),
-		point = ( $(blck).offset().top - 200 );
+	// $('body').on('click', '.k8-repl__link', function(e) {
+	// 	e.preventDefault();
+	// 	var blck = $(this).attr('href'),
+	// 	point = ( $(blck).offset().top - 200 );
 
-		// console.log( $(blck).offset().top );
-		// console.log( point );
+	// 	// console.log( $(blck).offset().top );
+	// 	// console.log( point );
 		
-		$('html, body').animate({
-      scrollTop: point
-    }, 2000);
-	});
+	// 	$('html, body').animate({
+ //      scrollTop: point
+ //    }, 2000);
+	// });
 
 });
