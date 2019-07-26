1ad6aa4ddf3abfe35eff7b71dd352cd257e5ce8a
dmitriiii <dimitriy1988wl@gmail.com>
2019-07-26 19:26:44 +0600

    pravka8

--- k8/assets/js/my.js
+++ k8/assets/js/my.js
@@ -2,10 +2,14 @@ jQuery(document).ready(function($){
 	// Scroll to block
 	$('body').on('click', '.k8-repl__link', function(e) {
 		e.preventDefault();
-		var blck = $(this).attr('href');
+		var blck = $(this).attr('href'),
+		point = ( $(blck).offset().top - 300 );
+
 		console.log( $(blck).offset().top );
+		console.log( point );
+		
 		$('html, body').animate({
-      scrollTop: $(blck).offset().top
+      scrollTop: point
     }, 2000);
 	});
 
