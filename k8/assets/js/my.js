db15194bef0e1f02c41d059d6637ea9515a01dd1
dmitriiii <dimitriy1988wl@gmail.com>
2019-07-26 19:18:08 +0600

    pravka6

--- k8/assets/js/my.js
+++ k8/assets/js/my.js
@@ -1,3 +1,11 @@
 jQuery(document).ready(function($){
-	console.log('readdyy');
+	// Scroll to block
+	$('body').on('click', '.k8-repl__link', function(e) {
+		e.preventDefault();
+		var blck = $(this).attr('href');
+		$('html, body').animate({
+      scrollTop: $(blck).offset().top
+    }, 2000);
+	});
+
 });
