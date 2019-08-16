47b27b7d644cfd7352c7ff1c81c1b35274c88d56
dmitriiii <dimitriy1988wl@gmail.com>
2019-08-16 14:44:12 +0600

    pr1

--- helper-classes/K8Short.php
+++ helper-classes/K8Short.php
@@ -4,7 +4,76 @@ class K8Short
 	public function __construct(){
 		#Show table with taxonomies Data
 		add_shortcode( 'k8_short_prod', array( $this, 'vpn_tax') );
+
+		#Show table with vpn details
+		add_shortcode( 'k8_short_vpndet', array( $this, 'vpn_det') );
+
 	}
+
+	public function vpn_det($atts){
+		// $arr = array(
+		// 	'Verbindungen pro Konto' => 'k8_acf_vpndet_conn',
+		// 	'Dauer(Monat)' => 'k8_acf_vpndet_durr1',
+		// 	'Dauer(Monat)' => 'k8_acf_vpndet_prc1',
+		// 	'k8_acf_vpndet_durr2',
+		// 	'k8_acf_vpndet_prc2',
+		// 	'k8_acf_vpndet_durr3',
+		// 	'k8_acf_vpndet_prc3',
+		// 	'k8_acf_vpndet_durr4',
+		// 	'k8_acf_vpndet_prc4',
+		// );
+		$pid = get_the_ID();
+		ob_start();?>
+			<table class="k8_compare-tbl">
+				<?php
+				if ( get_field( 'k8_acf_vpndet_conn', $pid ) ): ?>
+					<tr>
+						<td>Verbindungen pro Konto</td>
+						<td><?php the_field( 'k8_acf_vpndet_conn', $pid ); ?></td>
+					</tr>
+				<?php
+				endif;
+
+				if ( get_field( 'k8_acf_vpndet_durr1', $pid ) ): ?>
+					<tr>
+						<td>Dauer( <?php the_field( 'k8_acf_vpndet_durr1', $pid ); ?> Monat )</td>
+						<td><?php the_field( 'k8_acf_vpndet_prc1', $pid ); ?></td>
+					</tr>
+				<?php
+				endif;
+
+				if ( get_field( 'k8_acf_vpndet_durr2', $pid ) ): ?>
+					<tr>
+						<td>Dauer( <?php the_field( 'k8_acf_vpndet_durr2', $pid ); ?> Monat )</td>
+						<td><?php the_field( 'k8_acf_vpndet_prc2', $pid ); ?></td>
+					</tr>
+				<?php
+				endif;
+
+				if ( get_field( 'k8_acf_vpndet_durr3', $pid ) ): ?>
+					<tr>
+						<td>Dauer( <?php the_field( 'k8_acf_vpndet_durr3', $pid ); ?> Monat )</td>
+						<td><?php the_field( 'k8_acf_vpndet_prc3', $pid ); ?></td>
+					</tr>
+				<?php
+				endif;
+
+				if ( get_field( 'k8_acf_vpndet_durr4', $pid ) ): ?>
+					<tr>
+						<td>Dauer( <?php the_field( 'k8_acf_vpndet_durr4', $pid ); ?> Monat )</td>
+						<td><?php the_field( 'k8_acf_vpndet_prc4', $pid ); ?></td>
+					</tr>
+				<?php
+				endif;
+				?>
+
+			</table>
+		<?php
+	  $html = ob_get_clean();
+	  return $html;
+	}
+
+
 	public function vpn_tax($atts){
 		$arr = array(
 			'betriebssystem'=>'Betriebssysteme',
