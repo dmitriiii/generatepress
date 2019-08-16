783a77e017be967ddafe69ab006a1b59c354a03d
dmitriiii <dimitriy1988wl@gmail.com>
2019-08-16 18:04:00 +0600

    pr17

--- helper-classes/K8Short.php
+++ helper-classes/K8Short.php
@@ -96,6 +96,11 @@ class K8Short
 					<tr>
 						<td>Videoplattformen</td>
 						<td>
+							<?php 
+							echo '<pre style="display: none;">';
+							print_r( $videoplattformen );
+							echo '</pre>'; ?>
+
 							<?php
 							$ccc = 1;
 							foreach ($videoplattformen as $key=>$value): ?>
