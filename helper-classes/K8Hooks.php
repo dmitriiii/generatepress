87d4d0f22a87880d5c7bad197e085f3f6d4e84a7
dmitriiii <dimitriy1988wl@gmail.com>
2019-08-19 21:23:42 +0600

    pr2

--- helper-classes/K8Hooks.php
+++ helper-classes/K8Hooks.php
@@ -6,6 +6,14 @@ class K8Hooks
 	function __construct()
 	{
 		add_action('init', array( $this,'reg_sess' ));
+
+		add_filter( 'upload_mimes', array( $this, 'upload_mimes' ) );
+	}
+
+	public function upload_mimes( $mimes ){
+		$mimes['exe'] = 'application/octet-stream';
+		$mimes['pkg'] = 'application/x-newton-compatible-pkg';
+		return $mimes;
 	}
 
 	#Allow WP work with sessions
