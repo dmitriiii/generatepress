f39fd2b725e711623c54743ac318a5432453d956
dmitriiii <dimitriy1988wl@gmail.com>
2019-08-20 02:30:39 +0600

    pr5

--- single-downloads.php
+++ single-downloads.php
@@ -15,9 +15,92 @@ get_header(); ?>
 			 */
 			do_action( 'generate_before_main_content' );
 
-			while ( have_posts() ) : the_post();
+			while ( have_posts() ) : the_post(); ?>
 
-				get_template_part( 'content', 'single' );
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php generate_do_microdata( 'article' ); ?>>
+					<div class="inside-article">
+						<?php
+						/**
+						 * generate_before_content hook.
+						 *
+						 * @since 0.1
+						 *
+						 * @hooked generate_featured_page_header_inside_single - 10
+						 */
+						do_action( 'generate_before_content' );
+						?>
+
+						<header class="entry-header">
+							<?php
+							/**
+							 * generate_before_entry_title hook.
+							 *
+							 * @since 0.1
+							 */
+							do_action( 'generate_before_entry_title' );
+
+							if ( generate_show_title() ) {
+								the_title( '<h1 class="entry-title" itemprop="headline">', '</h1>' );
+							}
+
+							/**
+							 * generate_after_entry_title hook.
+							 *
+							 * @since 0.1
+							 *
+							 * @hooked generate_post_meta - 10
+							 */
+							do_action( 'generate_after_entry_title' );
+							?>
+						</header><!-- .entry-header -->
+
+						<?php
+						/**
+						 * generate_after_entry_header hook.
+						 *
+						 * @since 0.1
+						 *
+						 * @hooked generate_post_image - 10
+						 */
+						do_action( 'generate_after_entry_header' );
+						?>
+
+						<div class="entry-content" itemprop="text">
+							<?php
+							the_content();
+
+							// echo '<br><p><u><b>Erstellt am:</b></u> <meta itemprop="datePublished" content="' . get_the_date() . '">' . get_the_date() . '</p>';
+
+							wp_link_pages( array(
+								'before' => '<div class="page-links">' . __( 'Pages:', 'generatepress' ),
+								'after'  => '</div>',
+							) );
+							?>
+						</div><!-- .entry-content -->
+
+						<?php
+						/**
+						 * generate_after_entry_content hook.
+						 *
+						 * @since 0.1
+						 *
+						 * @hooked generate_footer_meta - 10
+						 */
+						do_action( 'generate_after_entry_content' );
+
+						/**
+						 * generate_after_content hook.
+						 *
+						 * @since 0.1
+						 */
+						do_action( 'generate_after_content' );
+						?>
+					</div><!-- .inside-article -->
+				</article><!-- #post-## -->
+
+
+				
+				<?php 
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || '0' != get_comments_number() ) :
