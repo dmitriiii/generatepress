11467f5be638681d073c82aa8f84be4fcff9d364
dmitriiii <dimitriy1988wl@gmail.com>
2019-09-06 10:42:23 +0600

    5

--- k8/assets/js/my.js
+++ k8/assets/js/my.js
@@ -1,16 +1,61 @@
 jQuery(document).ready(function($){
-	// Scroll to block
-	// $('body').on('click', '.k8-repl__link', function(e) {
-	// 	e.preventDefault();
-	// 	var blck = $(this).attr('href'),
-	// 	point = ( $(blck).offset().top - 200 );
 
-	// 	// console.log( $(blck).offset().top );
-	// 	// console.log( point );
+	// console.log(k8All);
+
+	// SLIDER
+	if( $('.k8-sl').length > 0 ){
 		
-	// 	$('html, body').animate({
- //      scrollTop: point
- //    }, 2000);
-	// });
+		$('.k8-sl').each(function(index, el) {
+			var $wrr = $(el).parents('.k8-sl__wrr'),
+					$prev = $wrr.find('.k8-sl__prev'),
+					$next = $wrr.find('.k8-sl__next');
+			
+			// console.log(el);
+
+			$(el).slick({
+			  infinite: true,
+			  slidesToShow: 3,
+			  slidesToScroll: 1,
+			  autoplay: true,
+			  prevArrow: $prev,
+			  nextArrow: $next,
+			  speed: 1000,
+			  autoplaySpeed: 3000,
+			  responsive: [
+			    {
+			      breakpoint: 768,
+			      settings: {
+			        arrows: false,
+			        centerMode: true,
+			        slidesToShow: 1
+			      }
+			    }
+		  	]
+			});
+		});
+	}
+	
+
+	// Succesfull captcha
+	function k8CaptchaSucc( $obj ){
+		var $form = $('.k8-capt__form');
+		$.ajax({
+			type: 'POST',
+			dataType: 'json',
+			url: k8All.ajaxurl,
+			data: {
+				'action': 'k8_ajx_captcha_succ',
+				'dataSubm' : $form.serializeArray()
+			},
+			success: function (data) {
+				$form.html(data.html);
+				// console.log(data);
+			}
+		});
+	}
+
+	window.k8CaptchaSucc = k8CaptchaSucc;
+
+
 
 });
