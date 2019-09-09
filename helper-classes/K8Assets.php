eaf88df3671ead70ea280c76c1ab7c88358d7c35
dmitriiii <dimitriy1988wl@gmail.com>
2019-09-10 00:30:03 +0600

    36

--- helper-classes/K8Assets.php
+++ helper-classes/K8Assets.php
@@ -8,18 +8,12 @@ class K8Assets
 
 	public function load_scripts() {
 
-		wp_enqueue_style( 'reacher89-my-css', get_template_directory_uri() . '/k8/assets/css/my.css', array(), rand(1,1000), 'all' );
-  	
-  	wp_register_script( 'reacher89-my-js', get_template_directory_uri() . '/k8/assets/js/my.js', array(), rand(1,1000), true );
-  	wp_localize_script( 'reacher89-my-js', 'k8All', array(
-	    'ajaxurl' => admin_url( 'admin-ajax.php' ),
-	  ));
-  	wp_enqueue_script('reacher89-my-js');
-
-
 		if( is_single() && get_post_type()=='downloads' ){
 			wp_register_script( 'k8-slick-js', get_template_directory_uri() . '/k8/assets/js/slick.min.js', array('jquery'), null, false );
 			wp_enqueue_script( 'k8-slick-js' );
+
+			wp_register_script( 'k8-sticky-js', get_template_directory_uri() . '/k8/assets/js/jquery.sticky-kit.min.js', array('jquery'), null, false );
+			wp_enqueue_script( 'k8-sticky-js' );
 
 	 	 	wp_register_style( 'k8-slick-css', get_template_directory_uri() . '/k8/assets/css/slick.css', array(), false, 'all' );
 			wp_enqueue_style( 'k8-slick-css' );
@@ -30,6 +24,18 @@ class K8Assets
 			wp_register_script( 'k8-recaptcha-js', 'https://www.google.com/recaptcha/api.js', array(), null, false );
 			wp_enqueue_script( 'k8-recaptcha-js' );
 		}
+
+
+		wp_enqueue_style( 'reacher89-my-css', get_template_directory_uri() . '/k8/assets/css/my.css', array(), rand(1,1000), 'all' );
+  	
+  	wp_register_script( 'reacher89-my-js', get_template_directory_uri() . '/k8/assets/js/my.js', array(), rand(1,1000), true );
+  	wp_localize_script( 'reacher89-my-js', 'k8All', array(
+	    'ajaxurl' => admin_url( 'admin-ajax.php' ),
+	  ));
+  	wp_enqueue_script('reacher89-my-js');
+
+
+		
 
 
 		// wp_register_style( 'k8-all-css', get_template_directory_uri() . '/k8/assets/css/all.css', array(), true, 'all' );
