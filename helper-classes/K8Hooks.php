66d7d983f333cd16cefdfe91b562575de065b2d7
dmitriiii <dimitriy1988wl@gmail.com>
2019-09-12 19:40:16 +0600

    1

--- helper-classes/K8Hooks.php
+++ helper-classes/K8Hooks.php
@@ -1,47 +1,41 @@
-<?php /**
- * USEFUL CUSTOM HOOKS
- */
+<?php
 class K8Hooks
 {
 	function __construct()
 	{
 		add_action('init', array( $this,'reg_sess' ));
-
 		add_filter( 'upload_mimes', array( $this, 'upload_mimes' ) );
 
-		#Async load for google captcha
-		// add_filter('clean_url', array( $this, 'enqueue_async' ), 11, 1);
+		# Add column with custom field vpn_id to Posts Admin Column
+		add_filter('manage_posts_columns', array( $this,'add_admin_column' ));
+		add_action('manage_posts_custom_column', array( $this,'add_admin_column_show' ), 10, 2);
 	}
-
-
 
 	public function upload_mimes( $mimes ){
 		$mimes['exe'] = 'application/octet-stream';
 		$mimes['pkg'] = 'application/x-newton-compatible-pkg';
 		$mimes['dmg'] = 'application/x-apple-diskimage';
 		$mimes['apk'] = 'application/vnd.android.package-archive';
-		
+
 		return $mimes;
 	}
 
 	#Allow WP work with sessions
 	public function reg_sess(){
-    if( !session_id() )
-        session_start();
+    if( !session_id() ){
+      session_start();
+    }
 	}
 
-	#Async load for google captcha
-	// public function enqueue_async( $url ){
-	// 	if ( strpos($url, '#asyncload')===false )
- //    { // not our file
- //        return $url;
- //    }
- //    else{
- //    	$url = str_replace('#asyncload', '', $url);
- //    	// Must be a ', not "!
- //    	return "$url' defer='defer' async='async";
- //    }
-    
-	// }
+	public function add_admin_column($column) {
+	  $column['k8vpn_id'] = '<span style="color: red; font-weight: bold;">VPN ID *</span>';
+	  return $column;
+	}
+	public function add_admin_column_show($column_name) {
+	  if ($column_name == 'k8vpn_id') {
+	    echo get_field('vpn_id');
+	  }
+	}
+
 }
 new K8Hooks();
