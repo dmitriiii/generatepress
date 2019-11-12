011f451e704f0173fb09c5b5a9f9bd89af3a8a98
dmitriiii <dimitriy1988wl@gmail.com>
2019-11-13 02:15:22 +0600

    def tpl

--- k8tpl-ip-test.php
+++ k8tpl-ip-test.php
@@ -5,85 +5,216 @@ if( !isset($_GET['ip']) || !filter_var($_GET['ip'], FILTER_VALIDATE_IP) || $_GET
 	header("Location:" . home_url( $wp->request ) ."?ip=" . $_SERVER['REMOTE_ADDR'] );
 	die;
 }
-get_header();
-if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<div class="k8-ip__wrr">
-		<h1 style="text-align: center;">
-			<?php the_title(); ?>
-		</h1>
-		<div class="k8_tbl-resp k8-ip__tbl">
-			<table class="k8_compare-tbl mtb-30">
-				<tr data-ip=''>
-					<th data-ip=''><?php the_post_thumbnail('medium'); ?></th>
-					<th>
-						<input class="k8-copy__inp" id="myInput" value="<?php echo home_url( $wp->request ) . '/?ip=' . $_GET['ip']; ?>" readonly>
-						<div class="k8-tooltip">
-							<button class="k8-copy__url dwnd__butt grn">
-								<span class="k8-tooltiptext" id="myTooltip">Copy to clipboard</span>
-								Copy Results
-							</button>
-						</div>
-					</th>
-				</tr>
-				<tr>
-					<td colspan="2">
-						<div class="k8-ip__map">
-							<img src="" alt="Your location" style="margin-left: auto; margin-right: auto; display: block;">
-						</div>
-					</td>
-				</tr>
-				<tr data-ip='query'>
-					<td>IP Address</td>
-					<td><strong></strong></td>
-				</tr>
-				<tr data-ip='reverse'>
-					<td>PTR</td>
-					<td><strong></strong></td>
-				</tr>
-				<tr data-ip='country'>
-					<td>Country</td>
-					<td><strong></strong></td>
-				</tr>
-				<tr data-ip='city'>
-					<td>City</td>
-					<td><strong></strong></td>
-				</tr>
-				<tr data-ip='lat'>
-					<td>Lattitude</td>
-					<td><strong></strong></td>
-				</tr>
-				<tr data-ip='lon'>
-					<td>Longitude</td>
-					<td><strong></strong></td>
-				</tr>
-				<tr data-ip='as'>
-					<td>ASN</td>
-					<td><strong></strong></td>
-				</tr>
-				<tr data-ip='isp'>
-					<td>ISP</td>
-					<td><strong></strong></td>
-				</tr>
-				<tr data-ip='proxy'>
-					<td>Proxy</td>
-					<td><strong></strong></td>
-				</tr>
-				<tr data-ip='mobile'>
-					<td>Mobile</td>
-					<td><strong></strong></td>
-				</tr>
-				<tr>
-					<td data-ip=''>IP type</td>
-					<td><strong>Residential (ISP/Broadband)</strong></td>
-				</tr>
-			</table>
-		</div>
-	</div>
+get_header();?>
+
+<div id="primary" <?php generate_do_element_classes( 'content' ); ?>>
+		<main id="main" <?php generate_do_element_classes( 'main' ); ?>>
+			<?php
+			/**
+			 * generate_before_main_content hook.
+			 *
+			 * @since 0.1
+			 */
+			do_action( 'generate_before_main_content' );
+
+			while ( have_posts() ) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php generate_do_microdata( 'article' ); ?>>
+					<div class="inside-article">
+						<?php
+						/**
+						 * generate_before_content hook.
+						 *
+						 * @since 0.1
+						 *
+						 * @hooked generate_featured_page_header_inside_single - 10
+						 */
+						do_action( 'generate_before_content' );
+						?>
+
+						<header class="entry-header">
+							<?php
+							/**
+							 * generate_before_entry_title hook.
+							 *
+							 * @since 0.1
+							 */
+							do_action( 'generate_before_entry_title' );
+
+							// if ( generate_show_title() ) {
+							// 	the_title( '<h1 class="entry-title" itemprop="headline">', '</h1>' );
+							// }
+
+							/**
+							 * generate_after_entry_title hook.
+							 *
+							 * @since 0.1
+							 *
+							 * @hooked generate_post_meta - 10
+							 */
+							// do_action( 'generate_after_entry_title' );
+							?>
+						</header><!-- .entry-header -->
+
+						<?php
+						/**
+						 * generate_after_entry_header hook.
+						 *
+						 * @since 0.1
+						 *
+						 * @hooked generate_post_image - 10
+						 */
+						do_action( 'generate_after_entry_header' );
+						?>
+
+						<div class="entry-content" itemprop="text">
+
+							<div class="k8-ip__wrr">
+								<h1 style="text-align: center;">
+									<?php the_title(); ?>
+								</h1>
+								<div class="k8_tbl-resp k8-ip__tbl">
+									<table class="k8_compare-tbl mtb-30">
+										<tr data-ip=''>
+											<th data-ip=''><?php the_post_thumbnail('medium'); ?></th>
+											<th>
+												<input class="k8-copy__inp" id="myInput" value="<?php echo home_url( $wp->request ) . '/?ip=' . $_GET['ip']; ?>" readonly>
+												<div class="k8-tooltip">
+													<button class="k8-copy__url dwnd__butt grn">
+														<span class="k8-tooltiptext" id="myTooltip">Copy to clipboard</span>
+														Copy Results
+													</button>
+												</div>
+											</th>
+										</tr>
+										<tr>
+											<td colspan="2">
+												<div class="k8-ip__map">
+													<img src="" alt="Your location" style="margin-left: auto; margin-right: auto; display: block;">
+												</div>
+											</td>
+										</tr>
+										<tr data-ip='query'>
+											<td>IP Address</td>
+											<td><strong></strong></td>
+										</tr>
+										<tr data-ip='reverse'>
+											<td>PTR</td>
+											<td><strong></strong></td>
+										</tr>
+										<tr data-ip='country'>
+											<td>Country</td>
+											<td><strong></strong></td>
+										</tr>
+										<tr data-ip='city'>
+											<td>City</td>
+											<td><strong></strong></td>
+										</tr>
+										<tr data-ip='lat'>
+											<td>Lattitude</td>
+											<td><strong></strong></td>
+										</tr>
+										<tr data-ip='lon'>
+											<td>Longitude</td>
+											<td><strong></strong></td>
+										</tr>
+										<tr data-ip='as'>
+											<td>ASN</td>
+											<td><strong></strong></td>
+										</tr>
+										<tr data-ip='isp'>
+											<td>ISP</td>
+											<td><strong></strong></td>
+										</tr>
+										<tr data-ip='proxy'>
+											<td>Proxy</td>
+											<td><strong></strong></td>
+										</tr>
+										<tr data-ip='mobile'>
+											<td>Mobile</td>
+											<td><strong></strong></td>
+										</tr>
+										<tr>
+											<td data-ip=''>IP type</td>
+											<td><strong>Residential (ISP/Broadband)</strong></td>
+										</tr>
+									</table>
+								</div>
+							</div>
+	
+							<?php
+							the_content();
+
+							echo '<br><p><u><b>Erstellt am:</b></u> <meta itemprop="datePublished" content="' . get_the_date() . '">' . get_the_date() . '</p>';
+
+							wp_link_pages( array(
+								'before' => '<div class="page-links">' . __( 'Pages:', 'generatepress' ),
+								'after'  => '</div>',
+							) );
+							?>
+						</div><!-- .entry-content -->
+
+						<?php
+						/**
+						 * generate_after_entry_content hook.
+						 *
+						 * @since 0.1
+						 *
+						 * @hooked generate_footer_meta - 10
+						 */
+						do_action( 'generate_after_entry_content' );
+
+						/**
+						 * generate_after_content hook.
+						 *
+						 * @since 0.1
+						 */
+						do_action( 'generate_after_content' );
+						?>
+					</div><!-- .inside-article -->
+				</article><!-- #post-## -->
+
+
+				<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || '0' != get_comments_number() ) :
+					/**
+					 * generate_before_comments_container hook.
+					 *
+					 * @since 2.1
+					 */
+					do_action( 'generate_before_comments_container' );
+					?>
+
+					<div class="comments-area">
+						<?php comments_template(); ?>
+					</div>
+
+					<?php
+				endif;
+
+			endwhile;
+
+			/**
+			 * generate_after_main_content hook.
+			 *
+			 * @since 0.1
+			 */
+			do_action( 'generate_after_main_content' );
+			?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
 	<?php
-	the_content();
-endwhile;
-else:
-endif;
+	/**
+	 * generate_after_primary_content_area hook.
+	 *
+	 * @since 2.0
+	 */
+	do_action( 'generate_after_primary_content_area' );
+
+	generate_construct_sidebars();
+
 get_footer();?>
 <script>
 	jQuery(document).ready(function($) {
