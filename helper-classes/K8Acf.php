d29330693f33e0515f1dac1da9c916188d2d20b2
dmitriiii <dimitriy1988wl@gmail.com>
2019-11-28 15:36:03 +0600

    faq

--- helper-classes/K8Acf.php
+++ helper-classes/K8Acf.php
@@ -9,10 +9,10 @@ class K8Acf
 		);
 		add_action( 'acf/init', array( $this, 'anbieter_cf' ) );
 		add_action( 'acf/init', array( $this, 'user_info' ) );
+		add_action( 'acf/init', array( $this, 'faq' ) );
 	}
 	public function anbieter_cf(){
 		if( function_exists('acf_add_local_field_group') ):
-
 			acf_add_local_field_group(array(
 				'key' => 'group_5c83071429237',
 				'title' => 'VPN Details',
@@ -557,14 +557,12 @@ class K8Acf
 				'active' => true,
 				'description' => '',
 			));
-
 		endif;
 	}
 
 	#USER INFO ( IMAGE with Description )
 	public function user_info(){
 		if( function_exists('acf_add_local_field_group') ):
-
 			acf_add_local_field_group(array(
 				'key' => 'group_5d1dc59e5ab2a',
 				'title' => 'User info',
@@ -631,8 +629,109 @@ class K8Acf
 				'active' => true,
 				'description' => '',
 			));
+		endif;
+	}
 
-			endif;
+	#FAQ Shortcode
+	public function faq(){
+		if( function_exists('acf_add_local_field_group') ):
+			acf_add_local_field_group(array(
+				'key' => 'group_5da9a88aa4335',
+				'title' => 'FAQ',
+				'fields' => array(
+					array(
+						'key' => 'field_5da9a89fb2b69',
+						'label' => 'Question&Answer',
+						'name' => 'k8_acf_faq',
+						'type' => 'repeater',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'collapsed' => '',
+						'min' => 0,
+						'max' => 0,
+						'layout' => 'table',
+						'button_label' => '',
+						'sub_fields' => array(
+							array(
+								'key' => 'field_5da9a8f6b2b6a',
+								'label' => 'Question',
+								'name' => 'quest',
+								'type' => 'text',
+								'instructions' => '',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'default_value' => '',
+								'placeholder' => '',
+								'prepend' => '',
+								'append' => '',
+								'maxlength' => '',
+							),
+							array(
+								'key' => 'field_5da9a90db2b6b',
+								'label' => 'Answer',
+								'name' => 'ans',
+								'type' => 'wysiwyg',
+								'instructions' => '',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'default_value' => '',
+								'tabs' => 'all',
+								'toolbar' => 'full',
+								'media_upload' => 1,
+								'delay' => 0,
+							),
+						),
+					),
+				),
+				'location' => array(
+					array(
+						array(
+							'param' => 'post_type',
+							'operator' => '==',
+							'value' => 'post',
+						),
+					),
+					array(
+						array(
+							'param' => 'post_type',
+							'operator' => '==',
+							'value' => 'page',
+						),
+					),
+					array(
+						array(
+							'param' => 'post_type',
+							'operator' => '==',
+							'value' => 'affcoups_coupon',
+						),
+					),
+				),
+				'menu_order' => 0,
+				'position' => 'normal',
+				'style' => 'default',
+				'label_placement' => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen' => '',
+				'active' => true,
+				'description' => '',
+			));
+		endif;
 	}
 }
 new K8Acf();
