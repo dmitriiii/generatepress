fd3e9b94222775612a21590df940adad560a0bd2
dmitriiii <dimitriy1988wl@gmail.com>
2019-11-28 14:21:47 +0600

    user info

--- helper-classes/K8Acf.php
+++ helper-classes/K8Acf.php
@@ -8,6 +8,7 @@ class K8Acf
 			'5dde6878b95b0'
 		);
 		add_action( 'acf/init', array( $this, 'anbieter_cf' ) );
+		add_action( 'acf/init', array( $this, 'user_info' ) );
 	}
 	public function anbieter_cf(){
 		if( function_exists('acf_add_local_field_group') ):
@@ -558,6 +559,80 @@ class K8Acf
 			));
 
 		endif;
+	}
+
+	#USER INFO ( IMAGE with Description )
+	public function user_info(){
+		if( function_exists('acf_add_local_field_group') ):
+
+			acf_add_local_field_group(array(
+				'key' => 'group_5d1dc59e5ab2a',
+				'title' => 'User info',
+				'fields' => array(
+					array(
+						'key' => 'field_5d2585c55287a',
+						'label' => 'User Photo',
+						'name' => 'k8_acf_u_ph',
+						'type' => 'image',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'return_format' => 'id',
+						'preview_size' => 'medium',
+						'library' => 'all',
+						'min_width' => '',
+						'min_height' => '',
+						'min_size' => '',
+						'max_width' => '',
+						'max_height' => '',
+						'max_size' => '',
+						'mime_types' => '',
+					),
+					array(
+						'key' => 'field_5d2585f95287b',
+						'label' => 'User Info',
+						'name' => 'k8_acf_u_desc',
+						'type' => 'textarea',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'placeholder' => '',
+						'maxlength' => '',
+						'rows' => '',
+						'new_lines' => '',
+					),
+				),
+				'location' => array(
+					array(
+						array(
+							'param' => 'user_role',
+							'operator' => '==',
+							'value' => 'all',
+						),
+					),
+				),
+				'menu_order' => 0,
+				'position' => 'normal',
+				'style' => 'default',
+				'label_placement' => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen' => '',
+				'active' => true,
+				'description' => '',
+			));
+
+			endif;
 	}
 }
 new K8Acf();
