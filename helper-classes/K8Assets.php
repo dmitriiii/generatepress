5d7e59798976576626877b5db14759bda2028839
dmitriiii <dimitriy1988wl@gmail.com>
2019-12-10 14:19:35 +0300

    css1

--- helper-classes/K8Assets.php
+++ helper-classes/K8Assets.php
@@ -26,6 +26,9 @@ class K8Assets
 		if( is_page_template( 'k8tpl-post-vpn-security.php' ) ){
 			wp_register_style( 'k8-boot4grid-css', get_template_directory_uri() . '/k8/assets/css/grid.min.css', array(), false, 'all' );
 			wp_enqueue_style( 'k8-boot4grid-css' );
+
+			wp_register_style( 'k8-vpn-security-css', get_template_directory_uri() . '/k8/assets/css/tpl/vpn-security.css', array(), false, 'all' );
+			wp_enqueue_style( 'k8-vpn-security-css' );
 		}
 
 		//SHORTCODE's JS & CSS
