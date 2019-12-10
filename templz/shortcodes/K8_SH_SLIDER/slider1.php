ad90966e5ea1d23c30935f594efc817e95901941
dmitriiii <dimitriy1988wl@gmail.com>
2019-12-10 18:15:34 +0300

    sldr shrt

--- templz/shortcodes/K8_SH_SLIDER/slider1.php
+++ templz/shortcodes/K8_SH_SLIDER/slider1.php
@@ -1,6 +1,10 @@
 <?php 
 if( isset( $rows ) && is_array( $rows ) && count( $rows ) > 0 ) : ?>
-	<div class="k8-sl__wrr k8-dwnd__sl w-scrns">
+	<div class="k8-sl__wrr k8-dwnd__sl w-scrns k8anim k8anim_brd">
+		<span class="k8anim_brd-item tp"></span>
+		<span class="k8anim_brd-item rght"></span>
+		<span class="k8anim_brd-item bttm"></span>
+		<span class="k8anim_brd-item lft"></span>
 		<div class="k8-sl__control k8-sl__prev"><i class="fas fa-chevron-left"></i></div>
 		<div class="k8-sl__control k8-sl__next"><i class="fas fa-chevron-right"></i></div>
 		<div class="k8-sl ">
