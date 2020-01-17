3ec3391ec12052f9c1e956cf6b6d18dce54f8f3b
dmitriiii <dimitriy1988wl@gmail.com>
2020-01-17 13:38:48 +0300

    lazyload Iframes

--- helper-classes/K8Acf.php
+++ helper-classes/K8Acf.php
@@ -11,6 +11,7 @@ class K8Acf
 		add_action( 'acf/init', array( $this, 'user_info' ) );
 		add_action( 'acf/init', array( $this, 'faq' ) );
 		add_action( 'acf/init', array( $this, 'slider' ) );
+		add_action( 'acf/init', array( $this, 'iframesAsync' ) );
 	}
 	public function anbieter_cf(){
 		if( function_exists('acf_add_local_field_group') ):
@@ -992,5 +993,80 @@ class K8Acf
 			));
 		endif;
 	}
+
+	#Iframes Async Load
+	public function iframesAsync(){
+		acf_add_local_field_group(array(
+			'key' => 'group_5e0a290ab20e5',
+			'title' => 'Iframes Async Load',
+			'fields' => array(
+				array(
+					'key' => 'field_5e0a2925377d2',
+					'label' => 'Url of short affiliate linlk',
+					'name' => 'k8_acf_ifr_url',
+					'type' => 'repeater',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'collapsed' => '',
+					'min' => 0,
+					'max' => 0,
+					'layout' => 'table',
+					'button_label' => '',
+					'sub_fields' => array(
+						array(
+							'key' => 'field_5e0a295b377d3',
+							'label' => 'Url',
+							'name' => 'url',
+							'type' => 'text',
+							'instructions' => '',
+							'required' => 0,
+							'conditional_logic' => 0,
+							'wrapper' => array(
+								'width' => '',
+								'class' => '',
+								'id' => '',
+							),
+							'default_value' => '',
+							'placeholder' => '',
+							'prepend' => '',
+							'append' => '',
+							'maxlength' => '',
+						),
+					),
+				),
+			),
+			'location' => array(
+				array(
+					array(
+						'param' => 'post_type',
+						'operator' => '==',
+						'value' => 'post',
+					),
+				),
+				array(
+					array(
+						'param' => 'post_type',
+						'operator' => '==',
+						'value' => 'page',
+					),
+				),
+			),
+			'menu_order' => 0,
+			'position' => 'normal',
+			'style' => 'default',
+			'label_placement' => 'top',
+			'instruction_placement' => 'label',
+			'hide_on_screen' => '',
+			'active' => true,
+			'description' => '',
+		));
+	}
+
 }
 new K8Acf();
