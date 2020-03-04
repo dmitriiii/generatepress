1b98ad1db4f8c710344f99ee3e24b75f0cc11868
dmitriiii <dimitriy1988wl@gmail.com>
2020-03-04 11:50:06 +0300

    change howto places

--- templz/shortcodes/K8_SH_HOWTO/design1.php
+++ templz/shortcodes/K8_SH_HOWTO/design1.php
@@ -20,14 +20,6 @@ if( is_array( $k8_acf_howto_stp ) && count( $k8_acf_howto_stp ) > 0 ) : ?>
 		$c = 1;
 		foreach ($k8_acf_howto_stp as $item): ?>
 			<div class="k8howto__item" id="howto_<?php echo $c; ?>">
-				<div class="k8howto__img k8-lg__wrr">
-					<a href="<?php echo K8Help::getImgUrl($item['img'],'full'); ?>" class="k8-lg__item k8howto__link" rel="nofollow">
-						<?php echo K8Html::getImgHtml([
-							'img_id' => $item['img'],
-							'size' => 'large',
-						]); ?>
-					</a>
-				</div>
 				<div class="k8howto__txt clearfix">
 					<div class="clearfix">
 						<div class="k8howto__num">
@@ -40,6 +32,14 @@ if( is_array( $k8_acf_howto_stp ) && count( $k8_acf_howto_stp ) > 0 ) : ?>
 					<div class="k8howto__txt2">
 						<?php echo $item['txt']; ?>
 					</div>
+				</div>
+				<div class="k8howto__img k8-lg__wrr">
+					<a href="<?php echo K8Help::getImgUrl($item['img'],'full'); ?>" class="k8-lg__item k8howto__link" rel="nofollow">
+						<?php echo K8Html::getImgHtml([
+							'img_id' => $item['img'],
+							'size' => 'large',
+						]); ?>
+					</a>
 				</div>
 			</div><!-- .k8howto__item -->
 		<?php 
