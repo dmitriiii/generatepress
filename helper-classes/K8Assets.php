be1f6e08ed7da5820ac64de353cab00f0ca4bc4b
dmitriiii <dimitriy1988wl@gmail.com>
2020-04-02 01:43:17 +0300

    table contents butt fix4

--- helper-classes/K8Assets.php
+++ helper-classes/K8Assets.php
@@ -19,7 +19,7 @@ class K8Assets
 		wp_register_style( 'k8-slick-css', get_template_directory_uri() . '/k8/assets/css/slick.css', array(), false, 'all' );
 		wp_register_style( 'k8-boot4grid-css', get_template_directory_uri() . '/k8/assets/css/grid.min.css', array(), false, 'all' );
 		wp_register_style( 'k8-vpn-security-css', get_template_directory_uri() . '/k8/assets/css/tpl/vpn-security.css', array(), false, 'all' );
-		wp_register_style( 'k8tpl-authors-css', get_template_directory_uri() . '/k8/assets/css/tpl/k8tpl-authors.css', array(), rand(1,9999), 'all' );
+		wp_register_style( 'k8tpl-authors-css', get_template_directory_uri() . '/k8/assets/css/tpl/k8tpl-authors.css', array(), false, 'all' );
 
 		wp_register_style( 'k8-ip-test-css', get_template_directory_uri() . '/k8/assets/css/tpl/ip-test.css', array(), false, 'all' );
 		wp_register_style( 'k8-libs-lightgallery-css', get_template_directory_uri() . '/k8/assets/css/libs/lightgallery.css', array(), false, 'all' );
@@ -41,7 +41,7 @@ class K8Assets
 
 		#Components&Modules
 		wp_register_script( 'k8-sidebar-nav-js', get_template_directory_uri() . '/k8/assets/js/components/sidebar-nav.js', array('jquery'), false, true );
-		wp_register_style( 'k8-sidebar-nav-css', get_template_directory_uri() . '/k8/assets/css/components/sidebar-nav.css', array(), false, 'all' );
+		wp_register_style( 'k8-sidebar-nav-css', get_template_directory_uri() . '/k8/assets/css/components/sidebar-nav.css', array(), rand(1,9999), 'all' );
 		#END Components&Modules
 		
 
@@ -57,14 +57,14 @@ class K8Assets
 		}
 
 		wp_enqueue_style( 'reacher89-fa-all-css', get_template_directory_uri() . '/k8/assets/css/fa-all.css', array(), false, 'all' );
-		wp_enqueue_style( 'reacher89-my-css', get_template_directory_uri() . '/k8/assets/css/my.css', array(), rand(1,9999), 'all' );
+		wp_enqueue_style( 'reacher89-my-css', get_template_directory_uri() . '/k8/assets/css/my.css', array(), false, 'all' );
 		
 		#Components&Modules
 		wp_enqueue_style( 'k8-sidebar-nav-css' );
 		wp_enqueue_script( 'k8-sidebar-nav-js' );
 		#END Components&Modules
 
-		wp_register_script( 'reacher89-my-js', get_template_directory_uri() . '/k8/assets/js/my.js', array(), rand(1,9999), true );
+		wp_register_script( 'reacher89-my-js', get_template_directory_uri() . '/k8/assets/js/my.js', array(), false, true );
 		wp_localize_script( 'reacher89-my-js', 'k8All', array(
 			'ajaxurl' => admin_url( 'admin-ajax.php' ),
 		));
