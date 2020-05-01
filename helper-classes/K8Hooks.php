557f1f814afd44c5e4107a7464693cb6175aff2a
dmitriiii <dimitriy1988wl@gmail.com>
2020-05-01 10:18:22 +0300

    add router ID column

--- helper-classes/K8Hooks.php
+++ helper-classes/K8Hooks.php
@@ -5,9 +5,10 @@ class K8Hooks
 	{
 		// add_action('init', array( $this,'reg_sess' ));
 		add_filter( 'upload_mimes', array( $this, 'upload_mimes' ) );
-		# Add column with custom field vpn_id to Posts Admin Column
+		# Add column with custom field vpn_id and m5_rou_id to Posts Admin Column
 		add_filter('manage_post_posts_columns', array( $this,'add_admin_column' ));
 		add_action('manage_post_posts_custom_column', array( $this,'add_admin_column_show' ), 10, 2);
+		
 		#Multiple Languages
 		add_action('after_setup_theme', array($this,'setup_theme'));
 		#Remove ACF for others
@@ -47,11 +48,15 @@ class K8Hooks
 	// }
 	public function add_admin_column($column) {
 		$column['k8_acf_vpnid'] = '<span style="color: red; font-weight: bold;">VPN ID *</span>';
+		$column['m5_rou_id'] = '<span style="color: red; font-weight: bold;">Router ID *</span>';
 		return $column;
 	}
 	public function add_admin_column_show($column_name) {
 		if ($column_name == 'k8_acf_vpnid') {
 			echo get_field('k8_acf_vpnid');
+		}
+		if ($column_name == 'm5_rou_id') {
+			echo get_field('m5_rou_id');
 		}
 	}
 	#Multiple Languages
