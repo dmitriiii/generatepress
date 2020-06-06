bf09aed3e87bed6e7e911a14d895488669a66ef8
dmitriiii <dimitriy1988wl@gmail.com>
2020-06-07 01:09:27 +0300

    rem amp sideb 2

--- amp/tpl/single-news.php
+++ amp/tpl/single-news.php
@@ -110,6 +110,7 @@ ob_start("k8_amp_callback");
 		<script data-k8req async src="https://cdn.ampproject.org/v0.js"></script>
 		<script data-k8req async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
 		<script data-k8req async custom-element="amp-sidebar" src="https://cdn.ampproject.org/v0/amp-sidebar-0.1.js"></script>
+		
 
 		<link rel="canonical" href="<?php echo $k8_can; ?>" data-k8req>
 		<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript data-k8req><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
