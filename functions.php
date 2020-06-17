1fd034b56c812da327214a6bdf10f12179893d0e
Tsvigun Evgeny <t1evgeny93@gmail.com>
2020-06-17 21:31:34 +0300

    Remove Gutenberg CSS from loading on the frontend

--- functions.php
+++ functions.php
@@ -212,3 +212,11 @@ add_filter( 'term_description', 'do_shortcode' );
 //
 //
 
+/**
+ * Remove Gutenberg Block Library CSS from loading on the frontend
+ */
+function smartwp_remove_wp_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+}
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
