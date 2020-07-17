e9ed2969aaeb7e8d7e6c58c0368da80ddaf6d010
dmitriiii <dimitriy1988wl@gmail.com>
2020-07-17 14:26:40 +0300

    fix shortcode content v2

--- templz/shortcodes/K8_SH_POPUP/design1.php
+++ templz/shortcodes/K8_SH_POPUP/design1.php
@@ -10,7 +10,9 @@
 				<?= get_the_title( $atts['id'] ); ?>
 			</h2>
 			<div class="popup__content">
-				<p><?= get_the_content($atts['id']); ?></p>
+				<p>
+					<?php  //get_the_content($atts['id']); ?>
+				</p>
 				<div class="timer" data-date="<?
 					$date = new DateTime( get_field('m5_acf_pop_date_to', $atts['id']) );
 					$date->setTime(14, 55, 24);
