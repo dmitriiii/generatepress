8bd160968e4d7dcbf0cb4053ac5295c17a5ef036
dmitriiii <dimitriy1988wl@gmail.com>
2020-08-03 13:04:52 +0300

    remove product review table in popup

--- helper-classes/K8Short.php
+++ helper-classes/K8Short.php
@@ -747,15 +747,18 @@ class K8Short
 		if( 'publish' !== get_post_status ( $atts['id'] ) )
 			return __('Publish popup first, please' , 'k8lang_domain');
 
-			wp_enqueue_style( 'k8-popup' );
-			wp_enqueue_script( 'k8-popup' );
-			wp_enqueue_style( 'k8-timer' );
-			wp_enqueue_script( 'k8-timer' );
-			wp_enqueue_script( 'k8-sales-popup' );
+		global $wp_filter;
+		wp_enqueue_style( 'k8-popup' );
+		wp_enqueue_script( 'k8-popup' );
+		wp_enqueue_style( 'k8-timer' );
+		wp_enqueue_script( 'k8-timer' );
+		wp_enqueue_script( 'k8-sales-popup' );
 
 		ob_start();
+		add_filter('wppr_is_review_active', false);
 		include $this->templ_url . $tag . '/design1.php';
 		$html = ob_get_clean();
+    array_pop( $wp_filter['wppr_is_review_active']->callbacks );
 		return $html;
 	}
 
