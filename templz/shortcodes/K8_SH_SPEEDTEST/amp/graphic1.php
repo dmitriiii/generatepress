dea9108c198afb137d0db231f0338ea4d713b093
dmitriiii <dimitriy1988wl@gmail.com>
2020-08-07 15:14:03 +0300

    simplify speedtest html

--- templz/shortcodes/K8_SH_SPEEDTEST/amp/graphic1.php
+++ templz/shortcodes/K8_SH_SPEEDTEST/amp/graphic1.php
@@ -7,63 +7,49 @@ $k8_acf_vpndet_up = get_field( 'k8_acf_vpndet_up', $pid ); ?>
 	<div class="head">
 		<?php _e('Maximale Geschwindigkeit getestet' , 'k8lang_domain'); ?>
 	</div>
-	<div class="tbl">
-		<div class="tbl-row">
-			<div class="tbl-cell cell-1">
-				<div class="wrr">
-					<div class="head-1">
-						<i class="fas fa-cloud-download-alt"></i>
-						<?php _e('Download' , 'k8lang_domain'); ?>
-					</div>
-					<div class="head-2">
-						<strong class="k8anim k8anim_countup" id="cnt<?php echo uniqid(); ?>" data-from="0" data-to="<?php echo $k8_acf_vpndet_down; ?>" data-k8countup>
-							<?php echo $k8_acf_vpndet_down; ?>
-						</strong>
-					</div>
-					<div class="head-3">Kbps</div>
+	<div class="tbl-row">
+		<div class="tbl-cell cell-1">
+			<div class="wrr">
+				<div class="head-1">
+					<?php _e('Download' , 'k8lang_domain'); ?>
 				</div>
+				<strong class="k8anim k8anim_countup" id="cnt<?php echo uniqid(); ?>" data-from="0" data-to="<?php echo $k8_acf_vpndet_down; ?>" data-k8countup>
+					<?php echo $k8_acf_vpndet_down; ?>
+				</strong>
+				<div class="head-3">Kbps</div>
 			</div>
-			<div class="tbl-cell cell-2">
-				<div class="wrr">
-					<div class="head-1">
-						<i class="fas fa-redo-alt"></i>
-						<?php _e('Jitter' , 'k8lang_domain'); ?>
-					</div>
-					<div class="head-2">
-						<strong class="k8anim k8anim_countup" id="cnt<?php echo uniqid(); ?>" data-from="0" data-to="<?php echo $k8_acf_vpndet_jitter; ?>" data-k8countup>
-							<?php echo $k8_acf_vpndet_jitter; ?>
-						</strong>
-					</div>
-					<div class="head-3">Ms</div>
+		</div>
+		<div class="tbl-cell cell-2">
+			<div class="wrr">
+				<div class="head-1">
+					<?php _e('Jitter' , 'k8lang_domain'); ?>
 				</div>
+				<strong class="k8anim k8anim_countup" id="cnt<?php echo uniqid(); ?>" data-from="0" data-to="<?php echo $k8_acf_vpndet_jitter; ?>" data-k8countup>
+					<?php echo $k8_acf_vpndet_jitter; ?>
+				</strong>
+				<div class="head-3">Ms</div>
 			</div>
-			<div class="tbl-cell cell-3">
-				<div class="wrr">
-					<div class="head-1">
-						<i class="fas fa-redo-alt"></i>
-						<?php _e('Ping' , 'k8lang_domain'); ?>
-					</div>
-					<div class="head-2">
-						<strong class="k8anim k8anim_countup" id="cnt<?php echo uniqid(); ?>" data-from="0" data-to="<?php echo $k8_acf_vpndet_ping; ?>" data-k8countup>
-							<?php echo $k8_acf_vpndet_ping; ?>
-						</strong>
-					</div>
-					<div class="head-3">Ms</div>
+		</div>
+		<div class="tbl-cell cell-3">
+			<div class="wrr">
+				<div class="head-1">
+					<?php _e('Ping' , 'k8lang_domain'); ?>
 				</div>
+				<strong class="k8anim k8anim_countup" id="cnt<?php echo uniqid(); ?>" data-from="0" data-to="<?php echo $k8_acf_vpndet_ping; ?>" data-k8countup>
+					<?php echo $k8_acf_vpndet_ping; ?>
+				</strong>
+				<div class="head-3">Ms</div>
 			</div>
-			<div class="tbl-cell cell-4">
-				<div class="wrr">
-					<div class="head-1">
-						<i class="fas fa-cloud-upload-alt"></i>
-						<?php _e('Upload' , 'k8lang_domain'); ?>
-					</div>
-					<div class="head-2">
-						<strong class="k8anim k8anim_countup" id="cnt<?php echo uniqid(); ?>" data-from="0" data-to="<?php echo $k8_acf_vpndet_up; ?>" data-k8countup>
-							<?php echo $k8_acf_vpndet_up; ?>
-						</strong>
-					</div>
-					<div class="head-3">Kbps</div>
+		</div>
+		<div class="tbl-cell cell-4">
+			<div class="wrr">
+				<div class="head-1">
+					<?php _e('Upload' , 'k8lang_domain'); ?>
 				</div>
+				<strong class="k8anim k8anim_countup" id="cnt<?php echo uniqid(); ?>" data-from="0" data-to="<?php echo $k8_acf_vpndet_up; ?>" data-k8countup>
+					<?php echo $k8_acf_vpndet_up; ?>
+				</strong>
+				<div class="head-3">Kbps</div>
 			</div>
 		</div>
 	</div>
