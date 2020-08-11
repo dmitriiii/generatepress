de1838278ccaffd296a0edf34a2b730a5a987fdf
dmitriiii <dimitriy1988wl@gmail.com>
2020-08-11 13:01:07 +0300

    add rss

--- functions.php
+++ functions.php
@@ -129,7 +129,7 @@ function k8_disable_feed_for_pages() {
 	remove_action( 'wp_head', 'feed_links_extra', 3 );
 	remove_action( 'wp_head', 'feed_links', 2 );
 	remove_action( 'wp_head', 'rsd_link' );
-	if(home_url($_SERVER['REQUEST_URI']) != site_url('feed/')) {
+	if(!in_array($_SERVER['REQUEST_URI'],['/feed/','/blog/neuigkeiten/feed/'])) {
 		remove_action( 'do_feed_rdf',  'do_feed_rdf',  10, 1 );
 		remove_action( 'do_feed_rss',  'do_feed_rss',  10, 1 );
 		remove_action( 'do_feed_rss2', 'do_feed_rss2', 10, 1 );
