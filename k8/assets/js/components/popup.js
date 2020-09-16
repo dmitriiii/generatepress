5b5a2a75ce6d56d4b3a4552b80d5e5536ede2aa6
dmitriiii <dimitriy1988wl@gmail.com>
2020-09-16 20:19:14 +0300

    popup aff link in new window

--- k8/assets/js/components/popup.js
+++ k8/assets/js/components/popup.js
@@ -1,16 +1,29 @@
+window.m5OpenInNewTab = function (url) {
+  win2=window.open(url,"_blank","width=100,height=100,left=0,top=0,menubar=no,status=no",false);
+  win2.blur();
+  window.focus();
+  setTimeout(function(){
+    win2.close();
+  }, 4000);
+}
+
 jQuery(document).ready(function ($) {
-  $(document.body).on("click", ".popup__btn-close", function () {
+  $(document.body).on("click", ".popup__btn-close", function (e) {
+    e.preventDefault();
     closePopup(this.closest(".popup"));
+    m5OpenInNewTab( $(this).attr('href') );
   });
 
-  $(document.body).on("click", ".popup__link", function () {
+  $(document.body).on("click", ".popup__link", function (e) {
+    e.preventDefault();
     closePopup($(this).siblings(".popup"));
+    m5OpenInNewTab( $(this).attr('href') );
   });
 
   $(document.body).on("click", ".popup__button", function () {
     closePopup($(this).closest(".popup"));
   });
-  
+
 
   window.openPopup = function openPopup(popupEl) {
     $(popupEl).closest(".popup-wrapper").addClass("popup-wrapper--showed");
@@ -25,8 +38,5 @@ jQuery(document).ready(function ($) {
       $popupWrapper.removeClass("popup-wrapper--showed");
     });
     $popupWrapper.removeClass("popup-wrapper--opened");
-    setTimeout(function(){ 
-      location.reload();
-    }, 4000);
   }
 })
