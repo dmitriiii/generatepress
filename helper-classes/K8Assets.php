033fe2ec166302557d680edc06d54ce1e69b4669
dmitriiii <dimitriy1988wl@gmail.com>
2020-11-18 15:31:59 +0300

    minor style changes for filter v2

--- helper-classes/K8Assets.php
+++ helper-classes/K8Assets.php
@@ -52,7 +52,7 @@ class K8Assets
 		wp_register_script( 'k8_sh_coupon-js-3', get_template_directory_uri() . '/templz/shortcodes/K8_SH_COUPON/build/static/js/3.b9afe8b6.chunk.js', array(), false, true );
 		wp_register_script( 'k8_sh_coupon-js-main', get_template_directory_uri() . '/templz/shortcodes/K8_SH_COUPON/build/static/js/main.25d22711.chunk.js', array(), false, true );
 
-		wp_register_style( 'k8_sh_coupon-css-main', get_template_directory_uri() . '/templz/shortcodes/K8_SH_COUPON/build/static/css/main.6747081a.chunk.css', array(), rand(1,9999), 'all' );
+		wp_register_style( 'k8_sh_coupon-css-main', get_template_directory_uri() . '/templz/shortcodes/K8_SH_COUPON/build/static/css/main.92d489f5.chunk.css', array(), rand(1,9999), 'all' );
 
 		wp_register_style( 'k8-pupop', get_template_directory_uri() . '/k8/assets/css/components/pupop.css', array(), rand(1,9999), 'all' );
 		wp_register_style( 'k8-timer', get_template_directory_uri() . '/k8/assets/css/components/timer.css', array(), rand(1,9999), 'all' );
