7a59c63a9be850d012751578b3afc1fc06117142
Delave-las-Kure <t1evgeny93@gmail.com>
2021-12-17 02:00:00 +0300

    inline style overlap

--- js/fout-resolver.js
+++ js/fout-resolver.js
@@ -1,16 +1,38 @@
 (function () {
   if (!document.fonts || document.fonts.check('16px "Roboto Condensed"')) {
-    console.log('Roboto Condensed loaded from cache');
+    console.log("Roboto Condensed loaded from cache");
     return;
   }
-    
-    var fontRC = new FontFaceObserver('Roboto Condensed', {
-      weight: 400
-    });
-    
-    document.documentElement.classList.add('rc-font-inactive')
-    fontRC.load().then(function () {
-      console.log('Roboto Condensed font is available');
-      document.documentElement.classList.remove('rc-font-inactive')
-    });
+
+  var fontRC = new FontFaceObserver("Roboto Condensed", {
+    weight: 400,
+  });
+
+  addedStyles();
+
+  document.documentElement.classList.add("rc-font-inactive");
+  fontRC.load().then(function () {
+    console.log("Roboto Condensed font is available");
+    document.documentElement.classList.remove("rc-font-inactive");
+    removeStyles()
+  });
+
+  function addedStyles() {
+    const styleEl = document.createElement("style");
+    styleEl.classList.add("rc-font-style-inactive");
+    styleEl.innerHTML = `
+      body {
+        font-family: sans-serif;
+        font-size: 15px;
+        line-height: 1.7;
+        letter-spacing: -0.25px;
+      }`;
+    document.head.append(styleEl);
+  }
+
+  function removeStyles() {
+    const styleEl = document.querySelector(".rc-font-style-inactive")
+    if (styleEl) styleEl.remove()
+  }
+
 })();
