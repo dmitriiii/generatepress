0f4cb73488c7770151f47f3fe5686f6db2bc2c31
Delave-las-Kure <t1evgeny93@gmail.com>
2021-12-16 22:33:54 +0300

    check font loading

---
+++ js/fout-resolver.js
@@ -0,0 +1,16 @@
+(function () {
+  if (!document.fonts || document.fonts.check('16px "Roboto Condensed"')) {
+    console.log('Roboto Condensed loaded from cache');
+    return;
+  }
+    
+    var fontRC = new FontFaceObserver('Roboto Condensed', {
+      weight: 400
+    });
+    
+    document.documentElement.classList.add('rc-font-inactive')
+    fontRC.load().then(function () {
+      console.log('Roboto Condensed font is available');
+      document.documentElement.classList.add('rc-font-inactive')
+    });
+})();
