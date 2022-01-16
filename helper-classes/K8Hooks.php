653302e01ec47db624c040bb9a97528161517e6e
dmitriiii <dimitriy1988wl@gmail.com>
2022-01-16 17:41:20 +0300

    rmv gutenberg on widgets area

--- helper-classes/K8Hooks.php
+++ helper-classes/K8Hooks.php
@@ -33,6 +33,15 @@ class K8Hooks
 
 		#Custom image sizes
 		add_action( 'after_setup_theme', array($this, 'theme_setup') );
+
+
+
+		// Disables the block editor from managing widgets in the Gutenberg plugin.
+		add_filter( 'gutenberg_use_widgets_block_editor', '__return_false', 100 );
+		// Disables the block editor from managing widgets. renamed from wp_use_widgets_block_editor
+		add_filter( 'use_widgets_block_editor', '__return_false' );
+
+
 	}
 	public function upload_mimes( $mimes ){
 		$mimes['exe'] = 'application/octet-stream';
